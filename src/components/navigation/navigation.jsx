9ee78df80f9e4741839c5c2d4ff804006ef2a876
SerhiiBilyk <sergiybiluk@gmail.com>
2017-12-06 16:50:38 +0100

    navigation add initial state

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 import styles from './Navigation.scss';
 import CSSModules from 'react-css-modules';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Logo from './logo/logo.jsx';
 import Hamburger from './hamburger/hamburger.jsx';
 
-var navitems=['Home','Team','About','Testimonials','Contact','Offers']
-
+var navitems = [
+  'Home',
+  'Team',
+  'About',
+  'Testimonials',
+  'Contact',
+  'Offers'
+]
 
 export class Navigation extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      collapsed:true
+    this.state = {
+      collapsed: true,
+      initial:false
     }
-    this.handlerHamburger=this.handlerHamburger.bind(this);
+    this.handlerHamburger = this.handlerHamburger.bind(this);
   }
-  handlerHamburger(){
-    this.setState(prevState=>{
-      return{
-        collapsed:!prevState.collapsed
+  handlerHamburger() {
+    this.setState(prevState => {
+      return {
+        collapsed: !prevState.collapsed,
+        initial:true
       }
     })
   }
   render() {
+    /* Check if you need initial state*/
+    var {collapsed,initial}=this.state;
     return (
-      <div styleName='navigation'>
+      <div styleName={`navigation ${initial?collapsed?'collapsed':'expanded':'initial'} `} >
         <div styleName='logo'>
-        <Logo/>
-      </div>
-        <nav styleName='nav' role='navigation' aria-label='main menu'>
+          <Logo/>
+        </div>
+        <nav styleName={`nav `} role='navigation' aria-label='main menu'>
           <ul role='menubar'>
-            {navitems.map((elem,index)=>{
+            {navitems.map((elem, index) => {
               return (
                 <li key={index} role='none' styleName='navitem'>
                   <Link role='menuitem' to={`/${elem.toLowerCase()}`}>{elem}</Link>
@@ -41,9 +51,8 @@ export class Navigation extends React.Component {
           </ul>
         </nav>
         <div styleName='hamburger'>
-        <Hamburger change={this.handlerHamburger} collapsed={this.state.collapsed} />
-      </div>
-
+          <Hamburger change={this.handlerHamburger} collapsed={this.state.collapsed}/>
+        </div>
       </div>
     )
   }
